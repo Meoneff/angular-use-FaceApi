40add278c919aa60a44265e4c675c8563f2d3455
minhkhoa1311 <khoa.nm00716@sinhvien.hoasen.edu.vn>
2024-05-30 13:47:15 +0700

    update new

--- src/app/pages/statistical-attendance/statistical-attendance.component.ts
+++ src/app/pages/statistical-attendance/statistical-attendance.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import * as xlsx from 'xlsx';
+// import* as path from 'path';
+
 
 @Component({
   selector: 'app-statistical-attendance',
@@ -10,10 +13,29 @@ import { CommonModule } from '@angular/common';
 })
 export class StatisticalAttendanceComponent {
   attendancesToRender: any[] = [];
+  workSheetName = `Attendance_${new Date().toLocaleDateString().split('/').join('-')}`
+  filePath = `../../Attendance_${new Date().toLocaleDateString().split('/').join('-')}.xlsx`
+  workSheetColumnName = [
+    "ID",
+    "Name",
+    "Date"
+  ]
   click() {
     const  attendances = sessionStorage.getItem('attendances');
     const attendancesAfterParse = JSON.parse(attendances??"[]");
     console.log(attendancesAfterParse);
     this.attendancesToRender = attendancesAfterParse;
+    const workBook = xlsx.utils.book_new();
+    const workSheetData = [
+      this.workSheetColumnName,
+      ...attendancesAfterParse.map((user: { id: any; name: any; timeAttendance: any; }) => [user.id, user.name, user.timeAttendance])
+    ];
+    console.log(workSheetData);
+    const workSheet = xlsx.utils.aoa_to_sheet(workSheetData);
+    xlsx.utils.book_append_sheet(workBook, workSheet, this.workSheetName);
+    xlsx.writeFile(workBook, this.filePath);
+    console.log(workSheet);
+    // console.log(path.resolve(this.filePath))
   }
+  
 }
